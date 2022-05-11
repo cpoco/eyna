0e25a222560f72a4d8d779596d84c88ab1f97449
cpoco <67665629+cpoco@users.noreply.github.com>
2022-05-11 15:01:45 +0900

    パッケージ作成方法のリファクタリング

--- build/package.mjs
+++ build/package.mjs
@@ -1,13 +1,21 @@
 import electron_builder from "electron-builder"
-import process from "node:process"
 
 export async function Package() {
-	let op = { x64: true }
-	if (process.platform == "win32") {
-		op.win = ["7z"]
-	}
-	else if (process.platform == "darwin") {
-		op.mac = ["7z"]
-	}
-	return electron_builder.build(op)
+	return electron_builder.build({
+		config: {
+			asar: false,
+			files: [
+				"./app/**/*",
+				"./config/*.json",
+				"./extension/*.js",
+			],
+			win: {
+				target: ["7z"],
+			},
+			mac: {
+				target: ["7z"],
+				identity: null,
+			},
+		},
+	})
 }
