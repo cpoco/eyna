fbaa953362e28a0a9d92ab0a4dc3d5f71e79b6a6
cpoco <67665629+cpoco@users.noreply.github.com>
2022-05-29 14:11:56 +0900

    テストコード

--- module/native/test/test.mjs
+++ module/native/test/test.mjs
@@ -1,4 +1,5 @@
 import path from "node:path"
+import process from "node:process"
 import url from "node:url"
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
@@ -6,8 +7,15 @@ import module from "node:module"
 const require = module.createRequire(import.meta.url)
 const native = require(path.join(__dirname, "../build/Release/native.node"))
 
-const wd = path.join("C:", "Users", "Public", "eyna test")
-// const wd = path.join("/", "Users", "Shared", "eyna test")
+if (process.platform == "win32") {
+	var wd = path.join("C:", "Users", "Public", "eyna test")
+}
+else if (process.platform == "darwin") {
+	var wd = path.join("/", "Users", "Shared", "eyna test")
+}
+else {
+	process.exit()
+}
 
 console.log("isElevated", native.isElevated())
 
@@ -15,9 +23,11 @@ console.log("getVolume", await native.getVolume())
 
 console.log("getAttribute", await native.getAttribute(wd, ""))
 console.log("getAttribute", await native.getAttribute(wd, wd))
+console.log("getAttribute", await native.getAttribute(wd, path.join(wd, "..")))
 
 console.log("getDirectory", await native.getDirectory(wd, "", false, 0, null))
 console.log("getDirectory", await native.getDirectory(wd, wd, false, 0, null))
+console.log("getDirectory", await native.getDirectory(wd, path.join(wd, ".."), false, 0, null))
 
 /*
 const ID = 0
