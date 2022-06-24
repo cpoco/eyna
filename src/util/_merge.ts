6207f1307473db2504bb1beaa00015779afce2cc
cpoco <67665629+cpoco@users.noreply.github.com>
2022-06-24 17:11:31 +0900

    プロトタイプ汚染対策

--- src/util/_merge.ts
+++ src/util/_merge.ts
@@ -1,16 +1,25 @@
 import { isArray } from "@/util/_is_array"
 import { isDict } from "@/util/_is_dict"
 
-export function merge(target: unknown, source: unknown) {
-	if (isDict(target) && isDict(source) || isArray(target) && isArray(source)) {
-		for (const [k, v] of Object.entries(source)) {
-			_merge(target, k, v)
-		}
+export function merge(target: any, source: any) {
+	for (const [k, v] of Object.entries(source)) {
+		_merge(target, k, v)
 	}
 }
 
 function _merge(target: any, key: string, source: unknown) {
-	if (isDict(target[key]) && isDict(source) || isArray(target[key]) && isArray(source)) {
+	if (isDict(source)) {
+		if (!isDict(target[key])) {
+			target[key] = {}
+		}
+		for (const [k, v] of Object.entries(source)) {
+			_merge(target[key], k, v)
+		}
+	}
+	else if (isArray(source)) {
+		if (!isArray(target[key])) {
+			target[key] = []
+		}
 		for (const [k, v] of Object.entries(source)) {
 			_merge(target[key], k, v)
 		}
