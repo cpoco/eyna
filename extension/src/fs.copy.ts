91061f8c51b758b271eb2ae74d610d2f708d9947
cpoco <67665629+cpoco@users.noreply.github.com>
2022-07-07 17:41:18 +0900

    選択処理は未実装

--- extension/src/fs.copy.ts
+++ extension/src/fs.copy.ts
@@ -5,26 +5,32 @@ import { normalize } from "node:path/posix"
 		return
 	}
 
-	let src = ex.active.cursor[0]!.full
-	let dst = join(ex.target.wd, ex.active.cursor[0]!.rltv)
+	if (ex.active.select.length == 0) {
+		let src = ex.active.cursor[0]!.full
+		let dst = join(ex.target.wd, ex.active.cursor[0]!.rltv)
 
-	let prompt = await ex.dialog.opne({ type: "prompt", title: "copy", text: dst })
-	if (prompt == null || prompt.text == "") {
+		let prompt = await ex.dialog.opne({ type: "prompt", title: "copy", text: dst })
+		if (prompt == null || prompt.text == "") {
+			return
+		}
+		dst = prompt.text
+
+		if (await ex.filer.exists(dst)) {
+			await ex.dialog.opne({ type: "alert", title: "copy", text: "exists" })
+			return
+		}
+
+		let dirdst = dirname(normalize(dst))
+		if (!await ex.filer.exists(dirdst)) {
+			await ex.filer.mkdir(dirdst)
+		}
+
+		await ex.filer.copy(src, dst)
+	}
+	else {
+		await ex.dialog.opne({ type: "alert", title: "copy", text: "select copy is unimplemented" })
 		return
 	}
-	dst = prompt.text
-
-	if (await ex.filer.exists(dst)) {
-		await ex.dialog.opne({ type: "alert", title: "copy", text: "exists" })
-		return
-	}
-
-	let dirdst = dirname(normalize(dst))
-	if (!await ex.filer.exists(dirdst)) {
-		await ex.filer.mkdir(dirdst)
-	}
-
-	await ex.filer.copy(src, dst)
 
 	ex.filer.update()
 })
--- extension/src/fs.move.ts
+++ extension/src/fs.move.ts
@@ -5,26 +5,32 @@ import { normalize } from "node:path/posix"
 		return
 	}
 
-	let src = ex.active.cursor[0]!.full
-	let dst = join(ex.target.wd, ex.active.cursor[0]!.rltv)
+	if (ex.active.select.length == 0) {
+		let src = ex.active.cursor[0]!.full
+		let dst = join(ex.target.wd, ex.active.cursor[0]!.rltv)
 
-	let prompt = await ex.dialog.opne({ type: "prompt", title: "move", text: dst })
-	if (prompt == null || prompt.text == "") {
+		let prompt = await ex.dialog.opne({ type: "prompt", title: "move", text: dst })
+		if (prompt == null || prompt.text == "") {
+			return
+		}
+		dst = prompt.text
+
+		if (await ex.filer.exists(dst)) {
+			await ex.dialog.opne({ type: "alert", title: "move", text: "exists" })
+			return
+		}
+
+		let dirdst = dirname(normalize(dst))
+		if (!await ex.filer.exists(dirdst)) {
+			await ex.filer.mkdir(dirdst)
+		}
+
+		await ex.filer.move(src, dst)
+	}
+	else {
+		await ex.dialog.opne({ type: "alert", title: "move", text: "select move is unimplemented" })
 		return
 	}
-	dst = prompt.text
-
-	if (await ex.filer.exists(dst)) {
-		await ex.dialog.opne({ type: "alert", title: "move", text: "exists" })
-		return
-	}
-
-	let dirdst = dirname(normalize(dst))
-	if (!await ex.filer.exists(dirdst)) {
-		await ex.filer.mkdir(dirdst)
-	}
-
-	await ex.filer.move(src, dst)
 
 	ex.filer.update()
 })
