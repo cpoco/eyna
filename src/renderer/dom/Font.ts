7daad125e8f519c4e1fcd51b61973f200ab5998b
cpoco <67665629+cpoco@users.noreply.github.com>
2023-09-17 10:05:58 +0900

    リンクをフォントアイコンに変更

--- src/renderer/dom/Font.ts
+++ src/renderer/dom/Font.ts
@@ -2,8 +2,9 @@
 // https://github.com/microsoft/vscode-codicons/blob/main/dist/codicon.csv
 // https://microsoft.github.io/vscode-codicons/dist/codicon.html
 
-export const source_control = "\u{ea68}"
+// export const source_control = "\u{ea68}"
 export const sync = "\u{ea77}"
 export const sync_ignored = "\u{eb9f}"
 export const circle_slash = "\u{eabd}"
 export const history = "\u{ea82}"
+export const arrow_right = "\u{ea9c}"
--- src/renderer/fragment/filer/CellComponent.ts
+++ src/renderer/fragment/filer/CellComponent.ts
@@ -1,6 +1,7 @@
 import * as vue from "@/renderer/Vue"
 
 import * as Bridge from "@/bridge/Bridge"
+import * as Font from "@/renderer/dom/Font"
 import * as Unicode from "@/renderer/dom/Unicode"
 import * as SpinnerComponent from "@/renderer/fragment/filer/SpinnerComponent"
 import root from "@/renderer/Root"
@@ -141,7 +142,6 @@ export const V = vue.defineComponent({
 			class: {
 				"filer-cicon"?: boolean
 				"filer-cfile"?: boolean
-				"filer-clink"?: boolean
 				"filer-ctrgt"?: boolean
 
 				"c-warn"?: boolean
@@ -157,7 +157,6 @@ export const V = vue.defineComponent({
 
 		let icon: style = { class: { "filer-cicon": true } }
 		let name: style = { class: { "filer-cfile": true } }
-		let link: style = { class: { "filer-clink": true } }
 		let trgt: style = { class: { "filer-ctrgt": true } }
 
 		let ftype = this.file_type[0] ?? Native.AttributeFileType.none
@@ -246,15 +245,21 @@ export const V = vue.defineComponent({
 						this.is_link
 							? [
 								vue.h("span", icon, [
-									vue.h("img", { src: `eyna://icon?p=${encodeURIComponent(this.cell.attr[0]?.full ?? "")}` }),
+									vue.h("img", {
+										class: { "filer-cimg": true },
+										src: `eyna://icon?p=${encodeURIComponent(this.cell.attr[0]?.full ?? "")}`,
+									}),
 								]),
 								vue.h("span", name, Unicode.rol(this.cell.attr[0]?.rltv)),
-								vue.h("span", link, " -> "),
+								vue.h("span", icon, Font.arrow_right),
 								vue.h("span", trgt, Unicode.rol(this.cell.attr[0]?.link)),
 							]
 							: [
 								vue.h("span", icon, [
-									vue.h("img", { src: `eyna://icon?p=${encodeURIComponent(this.cell.attr[0]?.full ?? "")}` }),
+									vue.h("img", {
+										class: { "filer-cimg": true },
+										src: `eyna://icon?p=${encodeURIComponent(this.cell.attr[0]?.full ?? "")}`,
+									}),
 								]),
 								vue.h("span", name, Unicode.rol(this.cell.attr[0]?.rltv)),
 							],
