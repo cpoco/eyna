770941668ab22a1d6797ce263cc4221da165eba0
cpoco <67665629+cpoco@users.noreply.github.com>
2024-01-06 22:12:12 +0900

    実装中

--- extension/src/fs.move.ts
+++ extension/src/fs.move.ts
@@ -1,5 +1,5 @@
-// import { basename, dirname, join, normalize } from "node:path/posix"
-;(async (ex: Extension): Promise<void> => {
+import * as path from "node:path/posix"
+; (async (ex: Extension): Promise<void> => {
 	if (ex.active == null || ex.target == null || ex.active.cursor == null) {
 		return
 	}
@@ -12,14 +12,38 @@
 		? [ex.active.cursor]
 		: ex.active.select
 
-	const base = ex.active.wd
+	const src_base = ex.active.wd
+	const dst_base = ex.target.wd
+
 	for (const v of ls) {
-		if (v[0]!.file_type == 1) {
-			const dir = await ex.filer.find(v[0]!.full, ex.active.wd)
-			for (const v of dir.list) {
-				log(v.type, base, v.rltv)
+		if (v[0]!.file_type == AttributeFileType.directory) {
+			const find = await ex.filer.find(v[0]!.full, src_base)
+			for (const w of find.list) {
+				operation(ex, w, src_base, dst_base)
 			}
 		}
-		log(v[0]!.file_type, base, v[0]!.rltv)
+		operation(ex, { type: v[0]!.file_type, rltv: v[0]!.rltv }, src_base, dst_base)
 	}
 })
+
+async function operation(_ex: Extension, item: Item, src_base: string, dst_base: string): Promise<void> {
+	const base = item.type == AttributeFileType.directory
+		? item.rltv
+		: path.dirname(item.rltv)
+
+	const file = item.type == AttributeFileType.directory
+		? ""
+		: path.basename(item.rltv)
+
+	log({
+		src: src_base,
+		dst: dst_base,
+		type: item.type,
+		base: base,
+		file: file,
+	})
+}
+
+enum AttributeFileType {
+	directory = 1,
+}
