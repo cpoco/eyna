65ee54263e6e3cbb332bb3ddb8e4ba9bfdb0b073
cpoco <67665629+cpoco@users.noreply.github.com>
2024-03-22 17:58:45 +0900

    DateTime

---
+++ pkg/util/ts/DateTime.ts
@@ -0,0 +1,23 @@
+export function DateTime(sec: number): { date: string; time: string } {
+	if (sec == 0) {
+		return {
+			date: "----/--/--",
+			time: "--:--:--",
+		}
+	}
+
+	const d = new Date(sec * 1000)
+
+	return {
+		date: [
+			d.getFullYear(),
+			(d.getMonth() + 1).toString().padStart(2, "0"),
+			d.getDate().toString().padStart(2, "0"),
+		].join("/"),
+		time: [
+			d.getHours().toString().padStart(2, "0"),
+			d.getMinutes().toString().padStart(2, "0"),
+			d.getSeconds().toString().padStart(2, "0"),
+		].join(":"),
+	}
+}
