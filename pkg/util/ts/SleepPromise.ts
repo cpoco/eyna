f5a41220893eaa139f077ca876c9b29806606445
cpoco <67665629+cpoco@users.noreply.github.com>
2024-03-25 12:04:13 +0900

    リファクタリング

--- pkg/util/ts/SleepPromise.ts
+++ pkg/util/ts/SleepPromise.ts
@@ -1,7 +1,5 @@
 export function SleepPromise(ms: number): Promise<void> {
-	return new Promise((resolve, _reject) => {
-		setTimeout(() => {
-			resolve()
-		}, ms)
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms)
 	})
 }
