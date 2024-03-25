b3243fdc4ff5d6363193f935cbe23dcc7980c699
cpoco <67665629+cpoco@users.noreply.github.com>
2024-03-25 12:05:25 +0900

    getIconの並列実行数を制限

--- src/browser/Protocol.ts
+++ src/browser/Protocol.ts
@@ -1,6 +1,7 @@
 import * as electron from "electron"
 
 import * as Native from "@eyna/native/ts/browser"
+import * as Util from "@eyna/util"
 
 const schema = "eyna"
 
@@ -10,12 +11,20 @@ export class Protocol {
 	}
 
 	static handle() {
+		const requestMax = 2
+		let requestCount = 0
+
 		electron.protocol.handle(schema, async (req: Request): Promise<Response> => {
 			let u = new URL(req.url)
 			let p = u.searchParams.get("p") ?? ""
 
+			while (requestMax <= requestCount) {
+				await Util.SleepPromise(0)
+			}
+
 			console.log(`\u001b[33m[icon]\u001b[0m`, { u: u.href, p: p })
 
+			requestCount++
 			return Native.getIcon(p)
 				.then((icon: Buffer) => {
 					return new Response(
@@ -27,6 +36,9 @@ export class Protocol {
 							},
 						},
 					)
+				})
+				.finally(() => {
+					requestCount--
 				})
 		})
 	}
