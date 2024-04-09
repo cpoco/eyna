26ec6ace309b4bdb32f1df0f9522bf5700641d74
cpoco <67665629+cpoco@users.noreply.github.com>
2024-04-09 17:53:32 +0900

    テスト更新

--- pkg/native/test/test.mjs
+++ pkg/native/test/test.mjs
@@ -1,6 +1,8 @@
 import native from "@eyna/native/esm/index.mjs"
+import fs from "node:fs/promises"
 import path from "node:path"
 import process from "node:process"
+import timers from "node:timers/promises"
 
 if (process.platform == "win32") {
 	var wd = path.join("C:", "Users", "Public", "eyna test")
@@ -16,6 +18,8 @@ console.log("isElevated", native.isElevated())
 
 console.log("getVolume", await native.getVolume())
 
+console.log("exists", await native.exists(wd))
+
 console.log("getAttribute", await native.getAttribute(wd, ""))
 console.log("getAttribute", await native.getAttribute(wd + "/", ""))
 console.log("getAttribute", await native.getAttribute(wd, wd))
@@ -30,6 +34,11 @@ console.log("getDirectory", await native.getDirectory(wd + "/", wd, false, 0, nu
 console.log("getDirectory", await native.getDirectory(wd, path.join(wd, ".."), false, 0, null))
 console.log("getDirectory", await native.getDirectory(wd + "/", path.join(wd, ".."), false, 0, null))
 
+const data = await native.getIcon(wd)
+const png = path.join(wd, "test.png")
+await fs.writeFile(png, data)
+await native.moveToTrash(png)
+
 const ID = 0
 native.watch(
 	ID,
@@ -38,9 +47,6 @@ native.watch(
 		console.log("watch callback", id, depth, path)
 	},
 )
-setTimeout(() => {
-	native.unwatch(ID)
-}, 3000)
 
 await native.createDirectory(path.join(wd, "watch", "d", "d"))
 await native.createFile(path.join(wd, "watch", "f"))
@@ -48,3 +54,7 @@ await native.moveToTrash(path.join(wd, "watch", "f"))
 await native.moveToTrash(path.join(wd, "watch", "d", "d"))
 await native.moveToTrash(path.join(wd, "watch", "d"))
 await native.moveToTrash(path.join(wd, "watch"))
+
+await timers.setTimeout(3000)
+
+native.unwatch(ID)
