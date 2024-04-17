90786247fcc503c9541da121b6879202f80978b2
cpoco <67665629+cpoco@users.noreply.github.com>
2024-04-17 16:31:03 +0900

    node-gyp@10

--- scripts/_native.mjs
+++ scripts/_native.mjs
@@ -1,5 +1,5 @@
-import * as rebuild from "@electron/rebuild"
 import fse from "fs-extra"
+import child_process from "node:child_process"
 import module from "node:module"
 import path from "node:path"
 import * as perf_hooks from "node:perf_hooks"
@@ -13,29 +13,40 @@ const outdir = path.join(__build, "app")
 
 const electron = module.createRequire(import.meta.url)(path.join(__top, "node_modules/electron/package.json"))
 
-export async function Build(arch, release = true) {
+export async function Build(arch) {
 	let _time = perf_hooks.performance.now()
 	await fse.ensureDir(outdir)
-	return rebuild.rebuild({
-		buildPath: __top,
-		arch: arch,
-		electronVersion: electron.version,
-		onlyModules: ["native"],
-		force: true,
+
+	const cmd = [
+		"npx",
+		"node-gyp",
+		"rebuild",
+		`--target=${electron.version}`,
+		`--arch=${arch}`,
+		"--dist-url=https://electronjs.org/header",
+	]
+
+	await new Promise((resolve, reject) => {
+		child_process.exec(
+			cmd.join(" "),
+			{
+				cwd: path.join(__top, "node_modules/@eyna/native"),
+			},
+			(error, _stdout, _stderr) => {
+				if (error) {
+					reject(error)
+				}
+				else {
+					resolve()
+				}
+			},
+		)
 	})
-		.then(() => {
-			if (release) {
-				return fse.copyFile(
-					path.join(__top, "node_modules/@eyna/native/build/Release/native.node"),
-					path.join(outdir, "native.node"),
-				)
-			}
-			else {
-				return Promise.resolve()
-			}
-		})
-		.then(() => {
-			console.log(`native ${(perf_hooks.performance.now() - _time).toFixed(0)}ms`)
-			return Promise.resolve()
-		})
+
+	await fse.copyFile(
+		path.join(__top, "node_modules/@eyna/native/build/Release/native.node"),
+		path.join(outdir, "native.node"),
+	)
+
+	console.log(`native ${(perf_hooks.performance.now() - _time).toFixed(0)}ms`)
 }
