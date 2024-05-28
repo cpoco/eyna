07a0f3efd43c51cb43371f687373de0d9dc8b2e1
cpoco <67665629+cpoco@users.noreply.github.com>
2024-05-28 14:12:32 +0900

    Revert "リファクタリング"
    This reverts commit bdf2fa67f51c077885f37da9e10e73cabfc44298.

--- pkg/native/src/open_properties.hpp
+++ pkg/native/src/open_properties.hpp
@@ -20,9 +20,7 @@ void open_properties(const v8::FunctionCallbackInfo<v8::Value>& info)
 
 	#if _OS_WIN_
 
-		std::replace(abst.begin(), abst.end(), L'/', L'\\');
-
-		SHObjectProperties(NULL, SHOP_FILEPATH, abst.c_str(), NULL);
+		SHObjectProperties(NULL, SHOP_FILEPATH, abst.make_preferred().c_str(), NULL);
 
 	#elif _OS_MAC_
 
