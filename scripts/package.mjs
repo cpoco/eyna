3877d8399c32708324757e9e6465f90c998f80a3
cpoco <67665629+cpoco@users.noreply.github.com>
2024-06-16 11:12:09 +0900

    process.arch

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -1,6 +1,6 @@
 import * as pk from "./_package.mjs"
 
-const arch = process.argv?.[2] == "arm64" ? "arm64" : "x64"
+const arch = process.argv?.[2] ?? process.arch
 console.log(`package (${arch})\n`)
 
 pk.Package(arch)
