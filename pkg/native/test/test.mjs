9d56b3e14001ed3cb3654ac2900ae1fc7a0f0514
cpoco <67665629+cpoco@users.noreply.github.com>
2024-11-22 15:14:03 +0900

    テスト更新

--- pkg/native/test/test.mjs
+++ pkg/native/test/test.mjs
@@ -2,7 +2,7 @@ import native from "@eyna/native/lib/index.mjs"
 
 import assert from "node:assert"
 import fs from "node:fs/promises"
-import path from "node:path"
+import path from "node:path/posix"
 import timers from "node:timers/promises"
 
 if (process.platform == "win32") {
@@ -39,31 +39,93 @@ const main = async () => {
 		assert(d.full == root)
 		assert(d.base == "")
 		assert(0 < d.list.length)
+
 		const a = await native.getAttribute(root)
 		assert(0 < a.length)
+		assert(a[0].file_type == 1)
 		assert(a[0].full == root)
 		assert(a[0].base == "")
+		assert(a[0].rltv == root)
+		assert(a[0].name == "")
+		assert(a[0].stem == "")
+		assert(a[0].exte == "")
+		assert(a[0].link_type == 0)
+		assert(a[0].link == null)
+	}
+
+	{
+		const p1 = await native.getPathAttribute(wd)
+		const p2 = await native.getPathAttribute(wd + "/")
+		if (process.platform == "win32") {
+			assert(p1.length == 4)
+			assert(p1[0].full == root)
+			assert(p1[1].full == "C:/Users")
+			assert(p1[2].full == "C:/Users/Public")
+			assert(p1[3].full == "C:/Users/Public/eyna test")
+		}
+		else if (process.platform == "darwin") {
+			assert(p1.length == 4)
+			assert(p1[0].full == root)
+			assert(p1[1].full == "/Users")
+			assert(p1[2].full == "/Users/Shared")
+			assert(p1[3].full == "/Users/Shared/eyna test")
+		}
+		assert(p1.length == p2.length)
+		assert(p1[0].full == p2[0].full)
+		assert(p1[1].full == p2[1].full)
+		assert(p1[2].full == p2[2].full)
+		assert(p1[3].full == p2[3].full)
 	}
 
 	{
 		const a1 = await native.getAttribute(wd)
 		const a2 = await native.getAttribute(wd + "/")
+		assert(a1[0].file_type == 1)
 		assert(a1[0].full == wd)
-		assert(a2[0].full == wd)
+		assert(a1[0].base == "")
+		assert(a1[0].rltv == wd)
+		assert(a1[0].name == "eyna test")
+		assert(a1[0].stem == "eyna test")
+		assert(a1[0].exte == "")
+		assert(a1[0].link_type == 0)
+		assert(a1[0].link == null)
+		assert(a1[0].file_type == a2[0].file_type)
+		assert(a1[0].full      == a2[0].full)
+		assert(a1[0].base      == a2[0].base)
+		assert(a1[0].rltv      == a2[0].rltv)
+		assert(a1[0].name      == a2[0].name)
+		assert(a1[0].stem      == a2[0].stem)
+		assert(a1[0].exte      == a2[0].exte)
+		assert(a1[0].link_type == a2[0].link_type)
+		assert(a1[0].link      == a2[0].link)
+	}
+
+	{
+		const p = wd + "/LINK/la_self_0"
+		const l = await native.getAttribute(p)
+		assert(l.length == 2)
+		assert(l[0].file_type == 2)
+		assert(l[0].full == p)
+		assert(l[0].base == "")
+		assert(l[0].rltv == p)
+		assert(l[0].name == "la_self_0")
+		assert(l[0].stem == "la_self_0")
+		assert(l[0].exte == "")
+		assert(l[0].link_type == 1)
+		assert(l[0].link == wd + "/LINK")
 	}
 
 	for (const abst of [root, wd, wd + "/"]) {
 		for (const base of ["", root, wd, wd + "/", path.join(wd, ".."), path.join(wd, "..") + "/"]) {
 			const a = await native.getAttribute(abst, base)
 			const d = await native.getDirectory(abst, base)
-			console.log(a, d)
 			assert(0 < a.length)
 			assert(0 < d.list.length)
 			assert(d.e == 0)
 			assert(a[0].full == d.full)
 			assert(a[0].base == d.base)
 			if (base == "") {
-				assert(a[0].rltv == a[0].full)
+				assert(a[0].full == a[0].rltv)
 			}
 			if (abst == base) {
 				assert(a[0].rltv == ".")
@@ -80,7 +142,6 @@ const main = async () => {
 
 	{
 		const ID = 0
-
 		native.watch(
 			ID,
 			wd,
