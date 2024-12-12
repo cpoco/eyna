9c0dc1318279c36ab9a2ec8a966d04f52ed15d1b
cpoco <67665629+cpoco@users.noreply.github.com>
2024-12-12 17:49:01 +0900

    DirDiff

---
+++ src/browser/core/DirDiff.ts
@@ -0,0 +1,73 @@
+import * as Native from "@eyna/native/lib/browser"
+
+type Diff = {
+	base: [string, string]
+	list: {
+		type: [Native.AttributeFileType, Native.AttributeFileType]
+		rltv: string
+	}[]
+	cnt: [number, number]
+}
+
+export class DirDiff {
+	static async diff(wd1: string, wd2: string, depth: number, cb: (diff: Diff) => void) {
+		const all = await Promise.all([
+			Native.getDirectory(wd1, wd1, false, depth),
+			Native.getDirectory(wd2, wd2, false, depth),
+		])
+
+		const diff: Diff = {
+			base: [wd1, wd2],
+			list: [],
+			cnt: [0, 0],
+		}
+
+		while (diff.cnt[0] < all[0].list.length && diff.cnt[1] < all[1].list.length) {
+			const data1 = all[0].list[diff.cnt[0]]!
+			const data2 = all[1].list[diff.cnt[1]]!
+
+			if (data1.rltv == data2.rltv) {
+				diff.list.push({
+					type: [data1.type, data2.type],
+					rltv: data1.rltv,
+				})
+				diff.cnt[0]++
+				diff.cnt[1]++
+			}
+			else if (data1.rltv < data2.rltv) {
+				diff.list.push({
+					type: [data1.type, Native.AttributeFileType.None],
+					rltv: data1.rltv,
+				})
+				diff.cnt[0]++
+			}
+			else {
+				diff.list.push({
+					type: [Native.AttributeFileType.None, data2.type],
+					rltv: data2.rltv,
+				})
+				diff.cnt[1]++
+			}
+		}
+
+		while (diff.cnt[0] < all[0].list.length) {
+			const data1 = all[0].list[diff.cnt[0]]!
+			diff.list.push({
+				type: [data1.type, Native.AttributeFileType.None],
+				rltv: data1.rltv,
+			})
+			diff.cnt[0]++
+		}
+
+		while (diff.cnt[1] < all[1].list.length) {
+			const data2 = all[1].list[diff.cnt[1]]!
+			diff.list.push({
+				type: [Native.AttributeFileType.None, data2.type],
+				rltv: data2.rltv,
+			})
+			diff.cnt[1]++
+		}
+
+		cb(diff)
+	}
+}
