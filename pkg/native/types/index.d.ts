793a2169e52a6c4f559ced6deddd98ae8735f599
cpoco <67665629+cpoco@users.noreply.github.com>
2025-11-25 18:35:42 +0900

    tsに切り替え

--- pkg/native/types/index.d.ts
+++ pkg/native/types/index.d.ts
@@ -1,10 +1,2 @@
 /// <reference types="./_type.d.ts" />
 /// <reference types="./_func.d.ts" />
-
-declare module "@eyna/native/lib/index.cjs" {
-	export = Native
-}
-
-declare module "@eyna/native/lib/index.mjs" {
-	export = Native
-}
