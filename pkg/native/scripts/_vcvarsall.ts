1fd4a49b6d9f1a4e278a8ef93b5d41ac69b76d7a
cpoco <67665629+cpoco@users.noreply.github.com>
2025-11-29 00:17:53 +0900

    検索パス

--- pkg/native/scripts/_vcvarsall.ts
+++ pkg/native/scripts/_vcvarsall.ts
@@ -2,13 +2,11 @@ import child_process from "node:child_process"
 import fs from "node:fs"
 import path from "node:path"
 
-const msvc32 = path.join("C:", "Program Files (x86)", "Microsoft Visual Studio", "2022")
-const msvc64 = path.join("C:", "Program Files", "Microsoft Visual Studio", "2022")
+const msvc = path.join("C:", "Program Files", "Microsoft Visual Studio", "2022")
 
-const github = path.join(msvc32, "BuildTools", "VC", "Auxiliary", "Build", "vcvarsall.bat")
-const enterprise = path.join(msvc64, "Enterprise", "VC", "Auxiliary", "Build", "vcvarsall.bat")
-const professional = path.join(msvc64, "Professional", "VC", "Auxiliary", "Build", "vcvarsall.bat")
-const community = path.join(msvc64, "Community", "VC", "Auxiliary", "Build", "vcvarsall.bat")
+const enterprise = path.join(msvc, "Enterprise", "VC", "Auxiliary", "Build", "vcvarsall.bat")
+const professional = path.join(msvc, "Professional", "VC", "Auxiliary", "Build", "vcvarsall.bat")
+const community = path.join(msvc, "Community", "VC", "Auxiliary", "Build", "vcvarsall.bat")
 
 const names = ["Path", "INCLUDE", "LIB", "LIBPATH"]
 
@@ -17,14 +15,13 @@ export async function setVsCmdEnv(): Promise<void> {
 		return Promise.resolve()
 	}
 
-	for (const bat of [github, enterprise, professional, community]) {
+	for (const bat of [enterprise, professional, community]) {
 		if (!fs.existsSync(bat)) {
 			continue
 		}
 
-		console.log(bat)
-
 		return new Promise<void>((resolve, reject) => {
+		console.log(`"${bat}" x64`)
 			child_process.exec(`"${bat}" x64 && set`, { shell: "cmd" }, (error, stdout, stderr) => {
 				if (error) {
 					console.error(stderr)
