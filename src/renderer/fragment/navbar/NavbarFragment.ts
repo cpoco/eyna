91cf99e622e4750a50fe4f40038ea95957566bf9
cpoco <67665629+cpoco@users.noreply.github.com>
2026-03-19 12:32:12 +0900

    非アクティブ時の色変更

--- src/renderer/fragment/navbar/NavbarFragment.ts
+++ src/renderer/fragment/navbar/NavbarFragment.ts
@@ -2,20 +2,23 @@ import * as vue from "@vue/runtime-dom"
 
 import * as Unicode from "@/renderer/dom/Unicode"
 import * as FilerProvider from "@/renderer/fragment/filer/FilerProvider"
+import * as SystemProvider from "@/renderer/fragment/system/SystemProvider"
 
 const TAG = "navbar"
 
 export const V = vue.defineComponent({
 	setup() {
+		const sys = SystemProvider.inject()
 		const filer = FilerProvider.inject()
 
 		return {
+			app: sys.reactive.app,
 			title: filer.reactive.title,
 		}
 	},
 
 	render() {
-		return vue.h(TAG, { class: { "navbar-fragment": true } }, [
+		return vue.h(TAG, { class: { "navbar-fragment": true, "navbar-fragment-inactive": !this.app.active } }, [
 			vue.h("div", { class: { "navbar-title": true } }, Unicode.highlight(this.title.wd, this.title.err)),
 		])
 	},
